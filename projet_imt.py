fcbefc2cae6f94311e3307dcf66eff0e9792b3f6
MAFTOUH Mohammed Amine <mohammed-amine.maftouh@imt-atlantique.net>
2025-03-03 13:35:50 +0000

    Add new file

---
+++ projet_imt.py
@@ -0,0 +1,14 @@
+# imports
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import ast
+import umap
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics import silhouette_score
+
+
+def main():
