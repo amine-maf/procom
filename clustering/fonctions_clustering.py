80f468e46b776a27bd926b0fca1abf5427654c8f
MAFTOUH Mohammed Amine <mohammed-amine.maftouh@imt-atlantique.net>
2025-03-03 12:06:06 +0000

    Edit fonctions_clustering.py

--- clustering/fonctions_clustering.py
+++ clustering/fonctions_clustering.py
@@ -1,29 +1,23 @@
-def agglomerative_clustering(reduced_embeddings, cluster_range=range(2, 21)):
+def agglomerative_clustering(reduced_embeddings, n_clusters):
     """
-    Teste l'Agglomerative Clustering avec différents nombres de clusters.
+    Applique l'Agglomerative Clustering avec un nombre fixe de clusters.
     
-    :param reduced_embeddings: Matrice des embeddings réduits
-    :param cluster_range: Intervalle du nombre de clusters à tester
-    :return: Liste des labels prédits
+    :param reduced_embeddings: embeddings
+    :param n_clusters: Nombre de clusters
+    :return: Labels prédits
     """
-    clustering_results = {}
-    for n_clusters in cluster_range:
-        agg_clustering = AgglomerativeClustering(n_clusters=n_clusters)
-        agg_labels = agg_clustering.fit_predict(reduced_embeddings)
-        clustering_results[n_clusters] = agg_labels
-    return clustering_results
+    agg_clustering = AgglomerativeClustering(n_clusters=n_clusters)
+    agg_labels = agg_clustering.fit_predict(reduced_embeddings)
+    return agg_labels
 
-def gaussian_mixture(reduced_embeddings, cluster_range=range(2, 21)):
+def gaussian_mixture(reduced_embeddings, n_clusters):
     """
-    Teste le modèle de mélange gaussien (GMM) avec différents nombres de clusters.
+    Applique le modèle de mélange gaussien (GMM) avec un nombre fixe de clusters.
     
-    :param reduced_embeddings: Matrice des embeddings réduits
-    :param cluster_range: Intervalle du nombre de clusters à tester
-    :return: Liste des labels prédits
+    :param reduced_embeddings: embeddings
+    :param n_clusters: Nombre de clusters
+    :return: Labels prédits
     """
-    clustering_results = {}
-    for n_clusters in cluster_range:
-        gmm = GaussianMixture(n_components=n_clusters, random_state=42)
-        gmm_labels = gmm.fit_predict(reduced_embeddings)
-        clustering_results[n_clusters] = gmm_labels
-    return clustering_results
+    gmm = GaussianMixture(n_components=n_clusters, random_state=42)
+    gmm_labels = gmm.fit_predict(reduced_embeddings)
+    return gmm_labels
