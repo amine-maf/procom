e29ebe3bafabb8e9eda750d697431287f47d3356
MAFTOUH Mohammed Amine <mohammed-amine.maftouh@imt-atlantique.net>
2025-03-05 11:17:11 +0000

    Update 5 files
    - /projet_imt/.gitkeep
    - /projet_imt.py
    - /clustering/__init__.py
    - /results/__init__.py
    - /utils/__init__.py

--- projet_imt.py
+++ projet_imt.py
@@ -1,5 +1,11 @@
 # imports
 
+from clustering.agg_clustering import *
+from clustering.mesures_clustering import *
+from results.affichage_messages_par_cluster import *
+from utils.reduction_dimesion import *
+
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,3 +18,13 @@ from sklearn.metrics import silhouette_score
 
 
 def main():
+    # lecture du fichier csv
+    df = load .....
+    # reduction des dimensions
+    reduced_embeddings = reduce_embeddings(df, n_components=50, random_state=42)
+    # clustering
+    labels = agglomerative_clustering(reduced_embeddings, n_clusters=14)
+    # metriques de clustering
+    silhouette_score = compute_silhouette_scores(reduced_embeddings, labels)
+    # afficher les resultats
+    messages_par_cluster= afficher_messages_par_cluster(df,labels)
